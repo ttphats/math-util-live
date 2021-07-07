62d5e4655251f2327ae440da08d57889c9b429b2
ttphats <skphat789@gmail.com>
2021-07-07 17:37:06 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -15,18 +15,22 @@ public class MathUtilLive {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         long result = MathUtil.getFactorial(5);
-        
+
         System.out.println("expected: 5! = 120; actual: " + result);
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(1));
-        
+
         //MathUtil.getFactorial(-5); ném ngoại lệ thật rồi, ai biểu đưa cà chớn chi!!!
+        
+        //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY - CODE LÊN SEVER 5:35PM 7/7/2021
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
         
         //kĩ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gi
         //expected value: 5! = 120
         //mày/hàm ai đó viết khi chạy thực tế là mấy -actual, giả sử 120
     }
-    
+
 }
